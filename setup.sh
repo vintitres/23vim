ad52db2e6281ae67a59d436a6041cb344dd7b162
Jakub Woyke <jaqb@sekta.pl>
2012-11-03 01:04:35 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 git submodule update --init --recursive &&
-if [ -e ~/.vim ]; then
-    mv ~/.vim ~/.vim_bak
-fi &&
+mv -f ~/.vim ~/.vim_bak_`date +%s`
 ln -s $PWD/.vim ~/.vim &&
-if [ -e ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc_bak
-fi &&
+mv -f ~/.vimrc ~/.vimrc_bak_`date +%s`
 ln -s $PWD/.vimrc ~/.vimrc &&
 vim +BundleInstall +qall
